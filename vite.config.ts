9f09a44731447036ba0cf58ec67f9e56fe83c5b2
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-04-17 14:16:27 +0300

    check images

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,61 @@ import viteImagemin from 'vite-plugin-imagemin';
 export default () => {
   return {
     plugins: [
-      // viteImagemin({
-      //   gifsicle: {
-      //     optimizationLevel: 7,
-      //     interlaced: false,
-      //   },
-      //   optipng: {
-      //     optimizationLevel: 7,
-      //   },
-      //   mozjpeg: {
-      //     quality: 20,
-      //   },
-      //   pngquant: {
-      //     quality: [0.8, 0.9],
-      //     speed: 4,
-      //   },
-      //   svgo: {
-      //     plugins: [
-      //       {
-      //         name: 'removeViewBox',
-      //       },
-      //       {
-      //         name: 'removeEmptyAttrs',
-      //         active: false,
-      //       },
-      //     ],
-      //   },
-      // }),
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 3,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 3,
+        },
+        mozjpeg: {
+          quality: 80,
+        },
+        pngquant: {
+          quality: [0.8, 0.9],
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false,
+            },
+          ],
+        },
+      }),
     ],
   };
 };
+
+// viteImagemin({
+//   gifsicle: {
+//     optimizationLevel: 7,
+//     interlaced: false,
+//   },
+//   optipng: {
+//     optimizationLevel: 7,
+//   },
+//   mozjpeg: {
+//     quality: 20,
+//   },
+//   pngquant: {
+//     quality: [0.8, 0.9],
+//     speed: 4,
+//   },
+//   svgo: {
+//     plugins: [
+//       {
+//         name: 'removeViewBox',
+//       },
+//       {
+//         name: 'removeEmptyAttrs',
+//         active: false,
+//       },
+//     ],
+//   },
+// }),
